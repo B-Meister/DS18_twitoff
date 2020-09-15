4a13e3ee11340e276998d0f11bb431cef23b6780
B-Meister <63935466+B-Meister@users.noreply.github.com>
2020-09-15 18:59:32 -0400

    class 332 work

--- twitoff/app.py
+++ twitoff/app.py
@@ -2,14 +2,16 @@
 
 # import flask
 from flask import Flask, render_template
-from .models import DB, User, insert_example_users, insert_tweets
+from .models import DB, User
+from .twitter import insert_example_users, add_or_update_user
 
 
 def create_app():
     app = Flask(__name__)
-
+    # making the database for the API so we can reference
     app.config['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///db.sqlite3'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     # initializes the database within our app
     DB.init_app(app)
 
@@ -18,12 +20,8 @@ def create_app():
     # listens for path '/' and executes function when heard
     @app.route('/')
     def root():
-        # 'cleans' the database
         DB.drop_all()
         DB.create_all()
-        insert_example_users()
-        insert_tweets()
-
         # a SELECT * query (in SQLAlchemy form)
         users = User.query.all()
         # rendering template that we created passing Home to template
@@ -33,12 +31,37 @@ def create_app():
     def working_or_nah():
         return f'Gang gang what up Twitoff Users \n Please enjoy this app!'
 
+    @app.route('/example')
+    def example():
+        insert_example_users()
+        return render_template('base.html', title='3 Users have been added', users=User.query.all())
+
     @app.route('/update')
     def update():
-        return "Can't update yet! Functionality coming soon"
+        print('Cannot add your own users at the moment. Coming Soon.')
+        return render_template('base.html', title='Updated Users', users=User.query.all())
 
     @app.route('/reset')
     def reset():
-        return "Bruh... all that work making the database, just to scrub it?"
+        # "cleans" the database
+        DB.drop_all()
+        DB.create_all()
+        return render_template('base.html', title='Reset database!')
+
+    @app.route('/user')
+    def user():
+        pass
+
+    @app.route('/compare')
+    def compare():
+        return f'Cannot compare yet. Functionality coming soon.'
+
+    @app.route('/party')
+    def party():
+        return render_template('party.html', title='PARTY TIME!')
+
+    @app.route('/showtweets')
+    def show_tweets():
+        _ = User()
 
     return app
--- twitoff/models.py
+++ twitoff/models.py
@@ -19,6 +19,7 @@ class Tweet(DB.Model):
     # Tweets has a max of 280 characters
     # Increased limits to 350 for all attachments/links/etc.
     text = DB.Column(DB.Unicode(350))
+    embedding = DB.Column(DB.PickleType, nullable=False)
 
     # Creating a user id that is the same as the one we created in User class, relates the database
     user_id = DB.Column(DB.BigInteger, DB.ForeignKey('user.id'), nullable=False)
@@ -29,30 +30,5 @@ class Tweet(DB.Model):
         return '-Tweet {}-'.format(self.text)
 
 
-def insert_example_users():
-    # creating base Twitter users
-    # IF we run multiple times then we will get error
-    # because these 2 users already exist in database
-    jack = User(id=1, name='jackblack')
-    elon = User(id=1, name='elonmusk')
-    DB.session.add(jack)
-    DB.session.add(elon)
-    DB.session.commit()
-
-
-def insert_tweets():
-    tweet1 = Tweet(id=1, text="What if we went to Pluto", user_id=1)
-    tweet2 = Tweet(id=2, text="Nachos are the best", user_id=2)
-    tweet3 = Tweet()
-    tweet4 = Tweet()
-    tweet5 = Tweet()
-    tweet6 = Tweet()
-    DB.session.add(tweet1)
-    DB.session.add(tweet2)
-    DB.session.add(tweet3)
-    DB.session.add(tweet4)
-    DB.session.add(tweet5)
-    DB.session.add(tweet6)
-    DB.session.commit()
-
-
+def tweetTest():
+    pass
